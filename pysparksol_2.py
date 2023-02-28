2e73887751dc6a0db90200e49e1b1f2565e328f7
Sudeep saxena <sudeepsaxena199409@gmail.com>
2023-02-28 19:38:51 +0530

    Update pysparksol_2.py
    added logging and exception

--- pysparksol_2.py
+++ pysparksol_2.py
@@ -1,15 +1,20 @@
 
 from pyspark.sql import SparkSession
+import logging as logger
 
 class CsvToDatabase:
     def __init__(self, csv_paths, db_host, db_schema):
         self.csv_paths = csv_paths
+        self.fileName = "file_to_log.log"
+	    self.logger=logger
+	    self.logger.basicConfig(level=logger.DEBUG,filename=self.fileName,filemode='w+',format=f'%(levelname)s - %(lineno)s = %(asctime)s - %(message)s')
         self.db_host = db_host
         self.db_schema = db_schema
         self.spark = SparkSession.builder.appName("CsvToDatabase").getOrCreate()
 
     def read_csv_files(self):
         df = self.spark.read.option("header", "true").csv(self.csv_paths)
+        self.logger.info("Csv's read successfully")
         return df
 
     def write_to_database(self, df, table_name):
@@ -20,8 +25,13 @@ class CsvToDatabase:
             "password": "your-password"
         }
         df.write.jdbc(url=db_url, table=table_name, mode="overwrite", properties=db_properties)
+        self.logger.info("written output to DB")
 
     def execute(self, table_name):
-        df = self.read_csv_files()
-        if(df.count()!=0):
-            self.write_to_database(df, table_name)
+        try:
+            df = self.read_csv_files()
+            if(df.count()!=0):
+                self.write_to_database(df, table_name)
+        except Exception as e:
+            self.logger.info("Error occurred",e)
+        
